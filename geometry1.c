67fb5d89095517f35bf1b467c2c6ba633a8092df
Egor <egor.beleva.04@mail.ru>
2023-03-21 22:30:57 +0700

    Remake project

--- geometry1.c
+++ geometry1.c
@@ -1,163 +1,382 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define SIZE 10
-struct Point
+#include "geometry.h"
+
+int choose(char x)
 {
-    float number_x;
-    float number_y;
-};
-struct Circle
+    if(x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9' || x == '.')
+        return 1;
+    else
+        return 0;
+}
+
+struct Object *figure_init(int quantity)
 {
-    char name[10];
-    struct Point point1;
-    struct Point point2;
-    struct Point point3;
-    struct Point point4;
-    float R;
-    float S;
-    float P;
-};
-void error_fun(FILE *geometry, long step_last, long step_in, int num)
+    struct Object *object;
+    object = malloc(sizeof(struct Object *) * quantity);
+    if(object)
+        return object;
+    printf("Ошибка выделения памяти!\n");
+    return NULL;
+}
+
+struct Object *figure_realloc(struct Object *object, int capacity)
 {
-    char buf[40];
-    rewind(geometry);
-    for(int i = 0; i < num + 1; i++)
-        fgets(buf, 39, geometry);
-    for(int i = 0; i < 39; i++) {
-        if(buf[i] == '\n')
-            buf[i] = '\0';
+    int k = 2;
+    object = realloc(object, sizeof(struct Object *) * capacity * k);
+    if(object)
+        return object;
+    else
+    {
+        fprintf(stderr, "Ошибка перевыделения памяти\n");
+        exit(EXIT_FAILURE);
     }
-    printf("%s\n", buf);
-    while(step_last != step_in) {
+}
+
+void error_locate(int column, char *str)
+{
+    printf("%s", str);
+    while(column > 1)
+    {
         printf(" ");
-        step_last--;
+        column--;
     }
     printf("^\n");
+
 }
-void error_full(FILE *geometry, char *c, long step_in, long step_last, int num)
+
+void error_full(int num, int column, char *c, char *str)
 {
-    if(num != 0)
-        step_last = step_last - 1;
-    else
-        step_last = step_last + 1;
-    error_fun(geometry, step_last, step_in, num);
-    printf("Error at column %ld: expected %s\n", step_last, c);
+    error_locate(column, str);
+    printf("Error at string %d, column %d: expected %s\n", num, column, c);
     exit(EXIT_FAILURE);
 }
-void process_circle(FILE *geometry, struct Circle *gcircle, int num, long step_in)
+
+void process_circle(struct Object *gcircle, int num, char *str)
 {
-    char s[2], e[2], p[2];
+    char name[6], *key, number1[6], number2[6], rad[6];
+    char s, p, e;
+    int i;
     float pi = 3.14;
-    long step_last;
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%s", s)) != 1)
+    key = "circle";
+    s = '(';
+    p = ',';
+    e = ')';
+    //step_in = ftell(geometry);
+    for(i = 0; i < 6; i++)
+        name[i] = str[i];
+    if((strcmp(name, key)) == 0)
     {
-        error_full(geometry, "'('", step_in, step_last, num);
+        sprintf(gcircle->name, "%s", name);
     }
     else
     {
-        if(strcmp(s, "(") != 0)
-        {
-            error_full(geometry, "'('", step_in, step_last, num);
-        }
+        i = 0;
+        while(name[i] == key[i])
+            i++;
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: expected 'circle' or 'trinagle'\n", num, i + 1);
+        exit(EXIT_FAILURE);
     }
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%f", &gcircle->point1.number_x)) != 1)
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != s)
     {
-        error_full(geometry, "'<double>'", step_in, step_last, num);
-    }
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%f", &gcircle->point1.number_y)) != 1)
-    {
-        error_full(geometry, "'<double>'", step_in, step_last, num);
-    }
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%s", p)) != 1)
-    {
-        error_full(geometry, "','", step_in, step_last, num);
+        error_full(num, i + 1, "(", str);
     }
     else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
     {
-        if(strcmp(p, ",") != 0)
-        {
-            error_full(geometry, "','", step_in, step_last, num);
-        }
+        if(choose(str[i]))
+            number1[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
     }
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%f", &gcircle->R)) != 1)
+    gcircle->point1.number_x = atof(number1);
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
     {
-        step_last = ftell(geometry);
-        error_full(geometry, "'<double>'", step_in, step_last, num);
+        if(choose(str[i]))
+            number2[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
     }
-    step_last = ftell(geometry);
-    if((fscanf(geometry, "%s", e)) != 1)
+    gcircle->point1.number_y = atof(number2);
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != p)
     {
-        error_full(geometry, "')'", step_in, step_last, num);
+        error_full(num, i + 1, ",", str);
     }
     else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
     {
-        if(strcmp(e, ")") != 0)
-        {
-            error_full(geometry, "')'", step_in, step_last, num);
-        }
+        if(choose(str[i]))
+            rad[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
     }
-    
+    gcircle->R = atof(rad);
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != e)
+    {
+        error_full(num, i + 1, ")", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != '\n' && str[i] != '\0' && str[i] != '\r')
+    {
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: unexpected token\n", num, i + 1);
+        exit(EXIT_FAILURE);
+    }
+
     gcircle->P = 2 * pi * gcircle->R;
     gcircle->S = pi * gcircle->R * gcircle->R;
 }
 
-int main(int argc, char *argv[])
+void process_trinagle(struct Object *trinagle, int num, char *str)
 {
-    FILE *geometry;
-    struct Circle gcircle[SIZE];
-    char str[10];
-    long step_in;
-    int num = 0;
-    int quantity;
-    int true = 0;
-    if(argc < 2)
+    char name[8], *key, number1_x[6], number1_y[6], number2_x[6], number2_y[6], number3_x[6], number3_y[6], number4_x[6], number4_y[6];
+    char s, p, e;
+    int i;
+    float x1, x2, x3, pr;
+
+    key = "trinagle";
+    s = '(';
+    p = ',';
+    e = ')';
+
+    for(i = 0; i < 8; i++)
+        name[i] = str[i];
+    if((strcmp(name, key)) == 0)
     {
-        fprintf(stderr, "Использование: %s имя_файла\n", argv[0]);
+        sprintf(trinagle->name, "%s", name);
+    }
+    else
+    {
+        i = 0;
+        while(name[i] == key[i])
+            i++;
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: expected 'circle' or 'trinagle'\n", num, i + 1);
         exit(EXIT_FAILURE);
     }
-    if((geometry = fopen(argv[1], "rb")) == NULL)
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != s)
     {
-        fprintf(stderr, "Не удается открыть файл \"%s\"\n", argv[1]);
-        exit(EXIT_FAILURE);
+        error_full(num, i + 1, "(", str);
     }
-    printf("\n\nObject = 'circle' '(' Point ',' Number ')'\n\
-    | 'triangle' '(' '(' Point ',' Point ',' Point ',' Point ')' ')'\n\
-    Point = Number Number\n\
-    Number = (* Floating-point number *)\n\n");
-    printf("Количество фигур для ввода(не больше 10): ");
-    if((true = scanf("%d", &quantity)) <= 0 || quantity >= 10)
-        exit(EXIT_FAILURE);
-    for(int i = 0; i < quantity; i++) {
-        step_in = ftell(geometry);
-        if((fscanf(geometry, "%s", str)) == 1)
-        {
-            if((strcmp(str, "circle")) == 0) {
-                sprintf(gcircle[num].name, "%s", str);
-                process_circle(geometry, &gcircle[num], num, step_in);
-                num++;
-            }
-            else
-            {
-                error_fun(geometry, step_in, step_in, num);
-                printf("Error at column %ld: expected 'circle'\n", step_in);
-                exit(EXIT_FAILURE);
-            }
-        }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != s)
+    {
+        error_full(num, i + 1, "(", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number1_x[y] = str[i];
         else
-        {
-            error_fun(geometry, step_in, step_in, num);
-            exit(EXIT_FAILURE);
-        }
-        printf("%d. %s", i + 1, gcircle[i].name);
-        printf(" ( %.1f %.1f , %.1f )\n", gcircle[i].point1.number_x, gcircle[i].point1.number_y, gcircle[i].R);
-        printf("P = %.4f\n", gcircle[i].P);
-        printf("S = %.4f\n\n", gcircle[i].S);
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point1.number_x = atof(number1_x);
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number1_y[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point1.number_y = atof(number1_y);
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != p)
+    {
+        error_full(num, i + 1, ",", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number2_x[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point2.number_x = atof(number2_x);
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number2_y[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point2.number_y = atof(number2_y);
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != p)
+    {
+        error_full(num, i + 1, ",", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number3_x[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point3.number_x = atof(number3_x);
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number3_y[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point3.number_y = atof(number3_y);
+
+     while(str[i] == ' ')
+        i++;
+    if(str[i] != p)
+    {
+        error_full(num, i + 1, ",", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number4_x[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point4.number_x = atof(number4_x);
+
+    if(trinagle->point1.number_x != trinagle->point4.number_x)
+    {
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: data compatibility error\n", num, i + 1);
+        exit(EXIT_FAILURE);
+    }
+
+    while(str[i] == ' ')
+        i++;
+    for(int y = 0; str[i] != ' ' && y < 6; y++, i++)
+    {
+        if(choose(str[i]))
+            number4_y[y] = str[i];
+        else
+            error_full(num, i + 1, "'<double>'", str);
+    }
+    trinagle->point4.number_y = atof(number4_y);
+
+    if(trinagle->point1.number_y != trinagle->point4.number_y)
+    {
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: data compatibility error\n", num, i + 1);
+        exit(EXIT_FAILURE);
+    }
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != e)
+    {
+        error_full(num, i + 1, ")", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != e)
+    {
+        error_full(num, i + 1, ")", str);
+    }
+    else
+        i++;
+
+    while(str[i] == ' ')
+        i++;
+    if(str[i] != '\n' && str[i] != '\0' && str[i] != '\r')
+    {
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: unexpected token\n", num, i + 1);
+        exit(EXIT_FAILURE);
+    }
+
+    x1 = sqrt(pow((trinagle->point1.number_x - trinagle->point2.number_x), 2) + pow((trinagle->point1.number_y - trinagle->point2.number_y), 2));
+    x2 = sqrt(pow((trinagle->point2.number_x - trinagle->point3.number_x), 2) + pow((trinagle->point2.number_y - trinagle->point3.number_y), 2));
+    x3 = sqrt(pow((trinagle->point3.number_x - trinagle->point4.number_x), 2) + pow((trinagle->point3.number_y - trinagle->point4.number_y), 2));
+
+    trinagle->P = x1 + x2 + x3;
+    pr = trinagle->P / 2;
+    trinagle->S = sqrt(pr * (pr - x1) * (pr - x2) * (pr - x3));
+}
+
+int process_object(struct Object *object, char *str, int num)
+{
+    int i = 0;
+
+    while(str[i] == ' ')
+        i++;
+    
+    if(str[i] == 'c')
+        process_circle(object, num, str);
+    else if (str[i] == 't')
+        process_trinagle(object, num, str);
+    else
+    {
+        error_locate(i + 1, str);
+        printf("Error at string %d, column %d: expected 'circle' or 'trinagle'\n", num, i + 1);
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
